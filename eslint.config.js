d24f0290e8afdf2c970f1a82f1f604aea46bbcb0
Nicola Truscello <nicola_truscello@sluz.ch>
2024-09-19 10:52:33 +0200

    formatter test

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,26 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
 
 export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  { rules: {     "no-unused-vars": "error",
-    "prefer-const": "error",
-    "eqeqeq": "error" } },
+  {
+    languageOptions:
+      {
+        globals:
+          globals.browser,
+      },
+  },
+  pluginJs
+    .configs
+    .recommended,
+  {
+    rules:
+      {
+        'no-unused-vars':
+          'error',
+        'prefer-const':
+          'error',
+        eqeqeq:
+          'error',
+      },
+  },
 ];
--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 import chalk from 'chalk';
 
-var myUnusedVariable = 'It works';
+var myUnusedVariable =
+  'It works';
 
-const val = 3
+const val = 3;
 
-if (3 === val){
-    console.log(chalk.blue(myUnusedVariable))
+if (
+  3 ===
+  val
+) {
+  console.log(
+    chalk.blue(
+      myUnusedVariable,
+    ),
+  );
 }
